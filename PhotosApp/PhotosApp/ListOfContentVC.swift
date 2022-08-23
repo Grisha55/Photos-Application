dd82efdea96f769b9f59d5fd21138ff4f51710e7
Grigory <grishavinyar@icloud.com>
2022-08-23 18:20:40 +0300

    Initial commit

---
+++ PhotosApp/PhotosApp/ListOfContentVC.swift
@@ -0,0 +1,20 @@
+//
+//  ListOfContent.swift
+//  PhotosApp
+//
+//  Created by Григорий Виняр on 23/08/2022.
+//
+
+import UIKit
+
+class ListOfContentVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .yellow
+  }
+
+
+}
+
